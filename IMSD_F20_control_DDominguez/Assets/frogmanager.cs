b9b4991abe0e2e3ac80a7bc15aca611b9ba91f7e
david dominguez <david.dominguez1@mail.citytech.cuny.edu>
2020-09-22 20:34:08 -0500

    frogger hw

---
+++ IMSD_F20_control_DDominguez/Assets/frogmanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frogmanager : MonoBehaviour
+{   
+
+    public GameObject frog;
+
+    public GameObject frogPrefab;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (frog != null)
+               Destroy(frog);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (frog == null)
+            frog = Instantiate(frogPrefab, new Vector3(0, -5, 0), Quaternion.identity);
+        }
+    }     
+}
